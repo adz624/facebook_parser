fd255db3b5eb08e0d4214c5ea8b06f9cb466eb13
Ethan Li <ethan@visionbundles.com>
2014-04-17 16:51:57 +0800

    Fix facebook parser

--- get_users.rb
+++ get_users.rb
@@ -1,34 +1,32 @@
 require 'fb_graph'
 require 'colorize'
+require './parse_facebook.rb'
 
 # token 常過期
-ACCESS_TOKEN = 'CAACEdEose0cBAFnqKTFBoNwTQ9R9bUxX7ujE6ZCn5HbCLCvZACoY2R4ZBvQJUsaKWlx0T2W9xlijh8Re4UIpMtaOKsl5CGraiZBh73Opar8GWoE1ZAKJzcolPH87lVLYNYx1RZAv31ZCS0tlhNEMf6xZAq3d8SCJybQE1tPD7FybDa33GtqXqE6V8yPc4AyDBy3ZAQhh8UuAXsgZDZD'
+ACCESS_TOKEN = 'CAACEdEose0cBANmpxLtqzVeLtnkatZABcA2aCKwPZCmWgZBSjQZCoTTaArsfqgJDcCVtvZCQQfPCPdhyKLguUKoETtwE1SUhPEcXcSsB9fiUZAPFnjIrHZBwS8kFmhhCp7DVxSoOMvmuokc0E3thmbyfFTp7ZBB01nEeLyNKYWBvV4SDsqUZAd62f4AomV5iqztRucxIGVsxe1QZDZD'
 fg = FbGraph::Page.fetch('THSRforsale', access_token: ACCESS_TOKEN)
 
 posts = fg.posts
 post_count = 0
-users_cache = {}
+# LOGGER = Logger.new('xxx.log')
 loop do
   posts.each do |post|
     # 不抓沒有回覆的文章
     next if post.comments.blank?
     post_count += 1
-    # puts "#{post.raw_attributes['message'][0..10]} ...".red
-    puts "CreatedTime: #{post.created_time.strftime('%Y-%m-%d %H:%M:%S')}".yellow
-    puts "Comments Count: #{post.comments.count}".blue
-
-    post.comments.each do |comment|
-      user = comment.raw_attributes['from']
-      users_cache[user['id']] = user['name']
-    end
+    puts "#{post.raw_attributes['message'][0..10].strip} ...".red
+    puts "Post Created Time: #{post.created_time.strftime('%Y-%m-%d %H:%M:%S')}".yellow
+    # puts "Comments Count: #{post.comments.count}".blue
+    ParseFacebook.parase_comments(post.comments)
+    ParseFacebook.get_likes(post.likes)
     puts '----------------------------------------'
   end
   
   posts = posts.next
-  if posts.blank?
-    puts '-----------------End--------------------'
-    puts "Post Count: #{post_count}".yellow
-    File.open('facebook_users.rtf', 'w') { |file| file.write(users_cache) }
-    break
-  end
+  break if posts.blank?
 end
+
+puts '-----------------End--------------------'
+puts "Post Count: #{post_count}".yellow
+File.open('facebook_users.txt', 'w') { |file| file.write(ParseFacebook.get_users.join("\n")) }
+
--- parse_users.rb
+++ parse_users.rb
@@ -6,7 +6,7 @@ exec('ruby get_users.rb') unless File.exists?('facebook_users.rtf')
 file = File.open('facebook_users.rtf')
 users = eval(file.first)
 
-users.each do |id, name|
-  puts "ID: #{id}, Name: #{name}"
+users.each do |fb_id|
+  puts "ID: #{fb_id}"
 end
 puts "User Count: #{users.count}".yellow
